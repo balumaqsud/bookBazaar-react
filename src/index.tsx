f306d5e963a96d0132367fd5691a373f21e445dc
balumaqsud <ollobergan008@mail.ru>
2025-06-05 05:21:24 +0900

    fix: file structure updated

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import "./css/index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
