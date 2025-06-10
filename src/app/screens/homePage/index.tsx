b909726c27bf72ded4ab16cbf79532de875c4250
balumaqsud <ollobergan008@mail.ru>
2025-06-10 14:10:13 +0900

    fix: updated brands section

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -3,12 +3,14 @@ import MainNav from "../../components/header/MainNav";
 import MainBanner from "./MainBanner";
 
 import "../../../css/home.css";
+import Brands from "./Brands";
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <MainNav />
       <MainBanner />
+      <Brands />
     </div>
   );
 };
