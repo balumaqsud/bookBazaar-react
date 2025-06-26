293b01beb91a545a48bd4ed4c51a02fbdbb2ff33
balumaqsud <ollobergan008@mail.ru>
2025-06-26 21:30:31 +0900

    fix: top users fix again

--- src/app/screens/homePage/top-user.tsx
+++ src/app/screens/homePage/top-user.tsx
@@ -2,12 +2,8 @@ import * as React from "react";
 import Avatar from "@mui/joy/Avatar";
 import Chip from "@mui/joy/Chip";
 import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import ButtonGroup from "@mui/joy/ButtonGroup";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
-import CardOverflow from "@mui/joy/CardOverflow";
-import CardActions from "@mui/joy/CardActions";
 import Typography from "@mui/joy/Typography";
 import { Container, Stack } from "@mui/material";
 
