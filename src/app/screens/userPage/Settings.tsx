2461451c0db2a63e8dbb6cfbb6a56114ac455eee
balumaqsud <ollobergan008@mail.ru>
2025-07-05 16:04:00 +0900

    fix: user page fixed

---
+++ src/app/screens/userPage/Settings.tsx
@@ -0,0 +1,169 @@
+import { Box } from "@mui/material";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import Button from "@mui/material/Button";
+import { useGlobals } from "../../hooks/useGlobals";
+import { useState } from "react";
+import { MemberUpdateInput } from "../../../libs/types/member";
+import { T } from "../../../libs/types/common";
+import {
+  sweetErrorHandling,
+  sweetTopSmallSuccessAlert,
+} from "../../../libs/sweetAlerts";
+import { Messages, serverApi } from "../../../libs/config";
+import MemberService from "../../services/MemberService";
+
+export function Settings() {
+  const { authMember, setAuthMember } = useGlobals();
+  const [file, setFile] = useState<string>(
+    authMember?.memberImage
+      ? `${serverApi}/${authMember.memberImage}`
+      : "/icons/default-user.svg"
+  );
+
+  const [memberUpdateInput, setMemberUpdateInput] = useState<MemberUpdateInput>(
+    {
+      memberNick: authMember?.memberNick,
+      memberEmail: authMember?.memberEmail,
+      memberAddress: authMember?.memberAddress,
+      memberImage: authMember?.memberImage,
+      memberDescription: authMember?.memberDescription,
+    }
+  );
+
+  //handlers
+  const memberNickHandler = (e: T) => {
+    memberUpdateInput.memberNick = e.target.value;
+    setMemberUpdateInput({ ...memberUpdateInput });
+  };
+
+  const memberEmailHandler = (e: T) => {
+    memberUpdateInput.memberEmail = e.target.value;
+    setMemberUpdateInput({ ...memberUpdateInput });
+  };
+
+  const memberAddressHandler = (e: T) => {
+    memberUpdateInput.memberAddress = e.target.value;
+    setMemberUpdateInput({ ...memberUpdateInput });
+  };
+
+  const memberDescHandler = (e: T) => {
+    memberUpdateInput.memberDescription = e.target.value;
+    setMemberUpdateInput({ ...memberUpdateInput });
+  };
+
+  const submitButton = async () => {
+    try {
+      if (!authMember) throw new Error(Messages.error2);
+      if (
+        memberUpdateInput.memberNick === "" ||
+        memberUpdateInput.memberEmail === "" ||
+        memberUpdateInput.memberAddress === "" ||
+        memberUpdateInput.memberDescription === " "
+      ) {
+        throw new Error(Messages.error3);
+      }
+
+      const member = new MemberService();
+      const result = await member.updateMember(memberUpdateInput);
+      setAuthMember(result);
+      sweetTopSmallSuccessAlert("successfully modified", 700);
+    } catch (error) {
+      console.log(error);
+      sweetErrorHandling(error).then();
+    }
+  };
+
+  const imageHandler = (e: T) => {
+    const file = e.target.files[0];
+    const fileType = file.type,
+      validate = ["JPG", "JPEG", "PNG"];
+    if (validate.includes(fileType)) {
+      sweetErrorHandling(Messages.error5);
+    } else {
+      memberUpdateInput.memberImage = file;
+      setMemberUpdateInput({ ...memberUpdateInput });
+      setFile(URL.createObjectURL(file));
+    }
+  };
+  return (
+    <Box className={"settings"}>
+      <Box className={"member-media-frame"}>
+        <img src={file} className={"mb-image"} alt="" />
+        <div className={"media-change-box"}>
+          <span>Upload image</span>
+          <p>JPG, JPEG, PNG formats only!</p>
+          <div className={"up-del-box"}>
+            <Button component="label" onChange={imageHandler}>
+              <CloudDownloadIcon />
+              <input type="file" hidden />
+            </Button>
+          </div>
+        </div>
+      </Box>
+      <Box className={"input-frame"}>
+        <div className={"long-input"}>
+          <label className={"spec-label"}>Username</label>
+          <input
+            className={"spec-input mb-nick"}
+            type="text"
+            placeholder={authMember?.memberNick}
+            value={memberUpdateInput.memberNick}
+            name="memberNick"
+            onChange={memberNickHandler}
+          />
+        </div>
+      </Box>
+      <Box className={"input-frame"}>
+        <div className={"short-input"}>
+          <label className={"spec-label"}>Phone</label>
+          <input
+            className={"spec-input mb-phone"}
+            type="text"
+            placeholder={
+              authMember?.memberEmail ? authMember?.memberEmail : "no phone"
+            }
+            value={memberUpdateInput.memberEmail}
+            name="memberEmail"
+            onChange={memberEmailHandler}
+          />
+        </div>
+        <div className={"short-input"}>
+          <label className={"spec-label"}>Address</label>
+          <input
+            className={"spec-input  mb-address"}
+            type="text"
+            placeholder={
+              authMember?.memberAddress
+                ? authMember.memberAddress
+                : "no address"
+            }
+            value={memberUpdateInput.memberAddress}
+            name="memberAddress"
+            onChange={memberAddressHandler}
+          />
+        </div>
+      </Box>
+      <Box className={"input-frame"}>
+        <div className={"long-input"}>
+          <label className={"spec-label"}>Description</label>
+          <textarea
+            className={"spec-textarea mb-description"}
+            placeholder={
+              authMember?.memberDescription
+                ? authMember.memberDescription
+                : "no description"
+            }
+            value={memberUpdateInput.memberDescription}
+            name="memberDesc"
+            onChange={memberDescHandler}
+          />
+        </div>
+      </Box>
+      <Box className={"save-box"}>
+        <Button variant={"contained"} onClick={submitButton}>
+          Save
+        </Button>
+      </Box>
+    </Box>
+  );
+}
--- src/app/screens/userPage/index.tsx
+++ src/app/screens/userPage/index.tsx
@@ -1,7 +1,90 @@
-import React from "react";
+import { Box, Container, Stack } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Settings } from "./Settings";
+import { useHistory } from "react-router-dom";
+import { useGlobals } from "../../hooks/useGlobals";
+import { serverApi } from "../../../libs/config";
+import { MemberType } from "../../../libs/data/enums/member.enum";
 
-const UserPage = () => {
-  return <div>UserPage</div>;
-};
+import "../../../css/userPage.css";
 
-export default UserPage;
+export default function UserPage() {
+  const { authMember } = useGlobals();
+  const history = useHistory();
+  if (!authMember) history.push("/");
+  return (
+    <div className={"user-page"}>
+      <Container>
+        <Stack className={"my-page-frame"}>
+          <Stack className={"my-page-left"}>
+            <Box display={"flex"} flexDirection={"column"}>
+              <Box className={"menu-name"}>Modify Member Details</Box>
+              <Box className={"menu-content"}>
+                <Settings />
+              </Box>
+            </Box>
+          </Stack>
+
+          <Stack className={"my-page-right"}>
+            <Box className={"order-info-box"}>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+              >
+                <div className={"order-user-img"}>
+                  <img
+                    src={
+                      authMember?.memberImage
+                        ? `${serverApi}/${authMember.memberImage}`
+                        : "/icons/default-user.svg"
+                    }
+                    className={"order-user-avatar"}
+                    alt=""
+                  />
+                  <div className={"order-user-icon-box"}>
+                    <img
+                      src={
+                        authMember?.memberType === MemberType.ADMIN
+                          ? "/icons/restaurant.svg"
+                          : "/icons/user-badge.svg"
+                      }
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <span className={"order-user-name"}>
+                  {authMember?.memberNick}
+                </span>
+                <span className={"order-user-prof"}>
+                  {" "}
+                  {authMember?.memberType}
+                </span>
+                <span className={"order-user-prof"}>
+                  {" "}
+                  {authMember?.memberAddress
+                    ? authMember.memberAddress
+                    : "no address"}
+                </span>
+              </Box>
+              <Box className={"user-media-box"}>
+                <FacebookIcon />
+                <InstagramIcon />
+                <TelegramIcon />
+                <YouTubeIcon />
+              </Box>
+              <p className={"user-desc"}>
+                {authMember?.memberDescription
+                  ? authMember.memberDescription
+                  : "No description"}
+              </p>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
