b7ad5a898ac93268c1f00f338ea4598990ca7838
balumaqsud <ollobergan008@mail.ru>
2025-08-26 01:53:49 +0900

    fix: last one

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 #production
 
 git reset --hard
-git checkout main
-git pull origin main
+git checkout master
+git pull origin master 
 
 npm i yarn -g 
 yarn global add serve
 yarn 
 yarn run build
 
-pm2 restart BookBazaar-react || pm2 start npx --name BookBazaar-react -- serve -s build -l 3000
+pm2 start "yarn run start:prod" --name=AURO-REACT
