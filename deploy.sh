f42edc68f8ca3895c57f6dae50facdd4303df189
balumaqsud <ollobergan008@mail.ru>
2025-08-26 00:54:25 +0900

    fix 2

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,9 @@ git reset --hard
 git checkout main
 git pull origin main
 
-# Install dependencies
 npm install -g yarn
-yarn global add serve
 yarn install
 yarn build
 
-# Restart PM2 process (use consistent name)
+# Start React frontend with PM2 using npx (ESM-safe)
 pm2 restart bookBazaar-react || pm2 start npx --name bookBazaar-react -- serve -s build -l 80
