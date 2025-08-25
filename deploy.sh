2116378c511ca320ed5cc5191a9f1b5ceecd2b71
balumaqsud <ollobergan008@mail.ru>
2025-08-26 01:08:10 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,8 @@ git checkout main
 git pull origin main
 
 npm install -g yarn
-yarn global remove serve
-yarn global add serve@13.0.2
 yarn install
 yarn build
 
-# Start React frontend with PM2 (CommonJS-safe)
-pm2 restart bookBazaar-react || pm2 start serve --name bookBazaar-react -- -s build -l 80
+# Start React frontend with PM2 (ESM-safe)
+pm2 restart bookBazaar-react || pm2 start npx --name bookBazaar-react -- serve -s build -l 80
