d79bf2cfc35122a8e94110080387a5a1df81fb5e
balumaqsud <ollobergan008@mail.ru>
2025-08-26 01:01:45 +0900

    fix: ddd

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,5 @@ yarn global add serve@13
 yarn install
 yarn build
 
-# Start React frontend with PM2 using npx (ESM-safe)
-pm2 restart bookBazaar-react || pm2 start npx --name bookBazaar-react -- serve -s build -l 80
-
+# Start React frontend with PM2 (CommonJS-safe)
+pm2 restart bookBazaar-react || pm2 start serve --name bookBazaar-react -- -s build -l 80
