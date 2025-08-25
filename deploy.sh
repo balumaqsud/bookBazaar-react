e2310c1b55536b2158966717899d4d7a2da80f59
balumaqsud <ollobergan008@mail.ru>
2025-08-26 00:06:34 +0900

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # PRODUCTION
-git reset --hard 
-git checkout main 
+git reset --hard
+git checkout main
 git pull origin main
 
+# Install dependencies
 npm install -g yarn
 yarn global add serve
-yarn
+yarn install
 yarn build
 
-pm2 restart BookBazaar || pm2 start serve --name=bookBazaar-react -- -s build -l 80
+# Restart PM2 process (use consistent name)
+pm2 restart bookBazaar-react || pm2 start serve --name=bookBazaar-react -- -s build -l 80
