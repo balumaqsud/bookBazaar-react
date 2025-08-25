990ad009b81cb59639840c0baca01c3adb0cb52f
balumaqsud <ollobergan008@mail.ru>
2025-08-26 00:00:34 +0900

    fix: deploy fix

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ yarn global add serve
 yarn
 yarn build
 
-pm2 restart BookBazaar || pm2 start serve --name=BookBazaar -- -s build -l 80
+pm2 restart BookBazaar || pm2 start serve --name=bookBazaar-react -- -s build -l 80
