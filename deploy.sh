b4e2976647f3add11fe557f1ac1e086b3c288809
balumaqsud <ollobergan008@mail.ru>
2025-08-25 22:17:21 +0900

    fix: deploy prod fix

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# PRODUCTION
+git reset --hard 
+git checkout main 
+git pull origin main
+
+
+npm i yarn -g
+yarn global add serve
+yarn
+yarn build
+
+pm2 start "yarn run start:prod" --name=BookBazaar
+
+
