f14cf4fe20893dc6d4ede1e76fd6f254a7f04987
balumaqsud <ollobergan008@mail.ru>
2025-08-26 02:56:56 +0900

    fixing

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# production deploy
+#production
 
-# go to project
-cd /home/bookBazaar/bookBazaar-react || exit
-
-# reset & pull latest
 git reset --hard
 git checkout master
-git pull origin master
+git pull origin master 
 
-# install yarn if not installed
-if ! command -v yarn &> /dev/null
-then
-    npm install -g yarn
-fi
+npm i yarn -g 
+yarn global add serve
+yarn 
+yarn run build
 
-# install dependencies
-yarn
-
-# install serve globally if not installed
-if ! command -v serve &> /dev/null
-then
-    yarn global add serve
-fi
-
-# build frontend
-yarn build
-
-# restart PM2 process safely
-pm2 delete bookbazaar-react || true
-pm2 start yarn --name bookbazaar-react -- run start:prod
-pm2 save
+pm2 start "yarn run start:prod" --name=BOOKBAZAAR-REACT
