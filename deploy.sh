9e56532dd136d1b15e59e6d9951df8b90ddf51ac
balumaqsud <ollobergan008@mail.ru>
2025-08-25 23:57:41 +0900

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ yarn global add serve
 yarn
 yarn build
 
-pm2 restart BookBazaar || pm2 start serve --name=BookBazaar -- -s build -l 3000
+pm2 restart BookBazaar || pm2 start serve --name=BookBazaar -- -s build -l 80
