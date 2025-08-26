2798c3a0cb426434f673d16824badf632faf5987
balumaqsud <ollobergan008@mail.ru>
2025-08-27 00:23:29 +0900

    node version specifed

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ npm i yarn -g
 yarn global add serve
 yarn
 yarn run build
-pm2 start "yarn run start:prod" --name=bookbazaar-react
+
+pm2 start "yarn run start:prod" --name bookbazaar-react --interpreter $(which node)
